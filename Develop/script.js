9e9d099e402940396522550c4401bacd04a6b9d9
Matt Habbo <matthabbo45@gmail.com>
2022-02-23 10:32:21 -0500

    all characters not displaying in window but displaying on console

--- Develop/script.js
+++ Develop/script.js
@@ -7,12 +7,6 @@ var specialChar = ["!", "@", "#", "$", "(", ")", "%", "&", ",", "*", "+", "-", "
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// declaration, set your declarations so we wil be able to determine parameters of the password
-// var confirmLength = "";
-// var confirmNumericCharacter;
-// var confirmSpecialCharacter;
-// var confirmLowerCase;
-// var confirmUpperCase;
 
 //we need to confirm how many characters the user would like in their password we need between 12 and 128
 function generatePassword() {
@@ -20,7 +14,7 @@ function generatePassword() {
   confirmLength = parseInt(confirmLength);
 
   //loop this is the answer is not within the given parameters, you can use a loop or if statements.
-  while(confirmLength <=11 || confirmLength >= 129) {
+  while (confirmLength <=11 || confirmLength >= 129) {
     window.alert("Password length must be between 12 and 128 characters. Please try again");
     //repeat prompt to confirm length
     var confirmLength = window.prompt("How many characters would you like your password to contain?  Must be between 12 and 128 characters.");
@@ -29,26 +23,30 @@ function generatePassword() {
     window.alert("Your password will be " + confirmLength + " characters long.");
 
     //now we want the user to set the parameters of the password, remember the user must select at least one
-    var confirmNumericCharacter = window.confirm("Click OK to confirm if you would like to include numeric characters.");
-    var confirmSpecialCharacter = window.confirm("Click OK to confirm if you would like to include special characters.");
+    var confirmNumericCharacter = window.confirm("Click OK if you would like to include numeric characters.");
+    var confirmSpecialCharacter = window.confirm("Click OK if you would like to include special characters.");
     var confirmLowerCase = window.confirm("Click OK if you would like to include lowercase letters.");
     var confirmUpperCase = window.confirm("Click OK if you would like to include uppercase letters.");
       // we create another loop so that the user must pick one, create the loop and repeat
-      while(confirmNumericCharacter === false && confirmSpecialCharacter === false && confirmLowerCase === false && confirmUpperCase === false) {
+      while (confirmNumericCharacter === false && confirmSpecialCharacter === false && confirmLowerCase === false && confirmUpperCase === false) {
         window.alert("You must choose at least one character.");
-        var confirmNumericCharacter = window.confirm("Click OK to confirm if you would like to include numeric characters.");
-        var confirmSpecialCharacter = window.confirm("Click OK to confirm if you would like to include special characters.");
+        var confirmNumericCharacter = window.confirm("Click OK if you would like to include numeric characters.");
+        var confirmSpecialCharacter = window.confirm("Click OK if you would like to include special characters.");
         var confirmLowerCase = window.confirm("Click OK if you would like to include lowercase letters.");
         var confirmUpperCase = window.confirm("Click OK if you would like to include uppercase letters.");
       }
 
-  var numericCharacters = ""
-  var specialCharacters = ""
-  var lowerCharacters = ""
-  var upperCharacters = ""
-
-
+      
       //set if statements for when the user wants to use numbers, special characters, lowercase or uppercase letters  
+
+   
+
+    for (var i = 0; i < confirmLength; i++) {
+
+      var numericCharacters = ""
+      var specialCharacters = ""
+      var lowerCharacters = ""
+      var upperCharacters = ""
 
       if (confirmNumericCharacter) {
         // this will select a random element inside of the number variable
@@ -66,22 +64,28 @@ function generatePassword() {
       if (confirmUpperCase) {
         upperCharacters = upper[Math.floor(Math.random() * upper.length)];
       }
-
-  var passwordCharacters = numericCharacters + specialCharacters + lowerCharacters + upperCharacters
-
-      console.log(passwordCharacters);
-
+    
+      var passwordCharacters = numericCharacters + specialCharacters + lowerCharacters + upperCharacters
       var yourPassword = ""
+    
+      yourPassword = passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+      console.log(yourPassword)
+       
+    }
+      return yourPassword;
+    
+  
+}
+
+
+
+
       
       // i equals 0 at the beginning of the for loop, i < array.length as long as i is less than array length and we continue looping
       //after each loop we increment by 1
-      for (var i = 0; i < confirmLength; i++) {
-        yourPassword = yourPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-        console.log(yourPassword)
-      }
-      return yourPassword;
+      
 
-}
+
 
 // Write password to the #password input
 function writePassword() {
